dfa26ba9ab5bdc20d44f5e3bf4b53140efcfa88b
krishnabalajiwork <krishnabalajiwork@gmail.com>
2025-07-29 17:27:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # ─── LAYOUT ─────────────────────────────────────────────────
-left, right = st.columns([1.5, 0.5])
+left, right = st.columns([0.5, 0.1])
 
 with left:
     st.markdown("### 📝 Enter your comment about BTS here...")
